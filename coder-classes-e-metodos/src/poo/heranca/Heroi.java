47b336e1d035e8566d072541ad82141f2e905f96
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-04-25 22:15:05 -0300

    heranca subescrita

--- coder-classes-e-metodos/src/poo/heranca/Heroi.java
+++ coder-classes-e-metodos/src/poo/heranca/Heroi.java
@@ -1,4 +1,12 @@
 package poo.heranca;
 
-public class Heroi extends Jogador{
+public class Heroi extends Jogador {
+
+    boolean atacar(Jogador oponente) {
+
+        boolean atacar1 = super.atacar(oponente);
+        boolean atacar2 = super.atacar(oponente);
+
+        return atacar1 || atacar2;
+    }
 }
--- coder-classes-e-metodos/src/poo/heranca/Jogo.java
+++ coder-classes-e-metodos/src/poo/heranca/Jogo.java
@@ -3,20 +3,28 @@ package poo.heranca;
 public class Jogo {
 
     public static void main(String[] args) {
-        var j1 =new Heroi();
-        j1.x = 10;
-        j1.y = 10;
+        var monstro = new Monstro();
+        monstro.x = 10;
+        monstro.y = 10;
 
-        var j2 =new Heroi();
-        j2.x = 10;
-        j2.y = 11;
+        var heroi = new Heroi();
+        heroi.x = 10;
+        heroi.y = 11;
 
-        System.out.println(j1.vida);
-        System.out.println(j2.vida);
+        System.out.println(monstro.vida);
+        System.out.println(heroi.vida);
 
-        j1.atacar(j2);
-        j2.atacar(j1);
-        System.out.println(j1.vida);
-        System.out.println(j2.vida);
+        monstro.atacar(heroi);
+        heroi.atacar(monstro);
+
+        monstro.atacar(heroi);
+        heroi.atacar(monstro);
+
+        monstro.andar(DirecaoEnum.NORTE);
+        monstro.atacar(heroi);
+        heroi.atacar(monstro);
+
+        System.out.println("Monstro: " + monstro.vida);
+        System.out.println("Heroi: " +heroi.vida);
     }
 }
