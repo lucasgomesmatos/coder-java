45ef35601886b2c822664fdbcc8d74a7753836a8
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-05-17 21:56:53 -0300

    getters e setters

---
+++ coder-classes-e-metodos/src/poo/encapsulamento/Pessoa.java
@@ -0,0 +1,18 @@
+package poo.encapsulamento;
+
+public class Pessoa {
+
+    private int idade;
+
+    public Pessoa(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
---
+++ coder-classes-e-metodos/src/poo/encapsulamento/PessoaTeste.java
@@ -0,0 +1,8 @@
+package poo.encapsulamento;
+
+public class PessoaTeste {
+    public static void main(String[] args) {
+        var p1 = new Pessoa(30);
+        System.out.println(p1.getIdade());
+    }
+}
