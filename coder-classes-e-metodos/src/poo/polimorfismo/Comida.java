b00d4bc7aec53af61f4d97e6bfc594e81a99c0ee
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-05-18 22:54:54 -0300

    polimorfismo

---
+++ coder-classes-e-metodos/src/poo/polimorfismo/Comida.java
@@ -0,0 +1,17 @@
+package poo.polimorfismo;
+
+public class Comida {
+    private double peso;
+
+    public Comida(double peso) {
+        this.peso = peso;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        if (peso >= 0) this.peso = peso;
+    }
+}
---
+++ coder-classes-e-metodos/src/poo/polimorfismo/Jantar.java
@@ -0,0 +1,17 @@
+package poo.polimorfismo;
+
+public class Jantar {
+
+    public static void main(String[] args) {
+        var convidado = new Pessoa(99);
+
+        Arroz a1 = new Arroz(0.250);
+        Feijao f2 = new Feijao(0.180);
+
+        System.out.println(convidado.getPeso());
+
+        convidado.comer(a1);
+        convidado.comer(f2);
+        System.out.println(convidado.getPeso());
+    }
+}
