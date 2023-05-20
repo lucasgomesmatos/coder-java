2b8b986937702e3a54c65417e03f06da712d5eb2
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-05-19 23:34:44 -0300

    static

--- coder-classes-e-metodos/src/poo/polimorfismo/Jantar.java
+++ coder-classes-e-metodos/src/poo/polimorfismo/Jantar.java
@@ -7,11 +7,13 @@ public class Jantar {
 
         Arroz a1 = new Arroz(0.250);
         Feijao f2 = new Feijao(0.180);
+        Sorvete s3 = new Sorvete(0.180);
 
         System.out.println(convidado.getPeso());
 
         convidado.comer(a1);
         convidado.comer(f2);
         System.out.println(convidado.getPeso());
+
     }
 }
