9dd9583451fc361de00181bfd8b9d013081f5520
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-06-10 23:00:57 -0300

    foreach method reference

--- lambdas/src/lambdas/foreachFuncional/Foreach.java
+++ lambdas/src/lambdas/foreachFuncional/Foreach.java
@@ -8,6 +8,10 @@ public class Foreach {
     public static void main(String[] args) {
         List<String> aprovados = Arrays.asList("Ana", "Bia", "Lia", "Gui");
 
+        System.out.println("\n Lambda #01...");
         aprovados.forEach(nome -> System.out.println(nome));
+
+        System.out.println("\nMethod Reference...");
+        aprovados.forEach(System.out::println);
     }
 }
