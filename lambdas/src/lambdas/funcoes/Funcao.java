e4d1de6449006aa0824a3a3ddd36db11a56b92de
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-06-25 22:37:57 -0300

    functions funcional

---
+++ lambdas/src/lambdas/funcoes/Funcao.java
@@ -0,0 +1,16 @@
+package lambdas.funcoes;
+
+import java.util.function.Function;
+
+public class Funcao {
+    public static void main(String[] args) {
+        Function<Integer, String> parOuImpar = numero -> numero % 2 == 0 ? "Par" : "Impar";
+
+        System.out.println(parOuImpar.apply(32));
+
+        Function<String, String> resultado = valor -> "O resultado é "+ valor;
+
+        String resultadoFinal = parOuImpar.andThen(resultado).apply(32);
+        System.out.println(resultadoFinal);
+    }
+}
