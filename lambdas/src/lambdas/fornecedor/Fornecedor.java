e171ccf22aaf7e76c97e7d848856acaf51aa454f
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-07-02 09:58:26 -0300

    supplier

---
+++ lambdas/src/lambdas/fornecedor/Fornecedor.java
@@ -0,0 +1,13 @@
+package lambdas.fornecedor;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+    public static void main(String[] args) {
+        Supplier<List<String>> umaLista = () -> Arrays.asList("Ana", "João", "José");
+
+        System.out.println(umaLista.get());
+    }
+}
