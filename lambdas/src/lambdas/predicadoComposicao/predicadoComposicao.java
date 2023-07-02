47fb1758772593946ed1987c14c4b43d27e4d057
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-07-02 09:24:54 -0300

    predicate 2

---
+++ lambdas/src/lambdas/predicadoComposicao/predicadoComposicao.java
@@ -0,0 +1,14 @@
+package lambdas.predicadoComposicao;
+
+import java.util.function.Predicate;
+
+public class predicadoComposicao {
+
+    public static void main(String[] args) {
+
+        Predicate<Integer> isPar = num -> num % 2 == 0;
+        Predicate<Integer> isTresDigitos = num -> num >= 100 && num <= 999;
+        System.out.println(isPar.or(isTresDigitos).test(122));
+
+    }
+}
