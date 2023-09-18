b075c19a9d274d1091f6f42ab008271eec0052ee
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-09-18 19:57:23 -0300

    stream

---
+++ stream/src/list_iterator_stream/ImprimindoObjetos.java
@@ -0,0 +1,29 @@
+package list_iterator_stream;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ImprimindoObjetos {
+    public static void main(String[] args) {
+        List<String> aprovados = Arrays.asList("Lucas", "Maria", "Ana");
+
+        System.out.println("Usando o foreach...");
+        for (String aprovado : aprovados) {
+            System.out.println(aprovado);
+        }
+
+        System.out.println("\nUsando Iterator...");
+        Iterator<String> iterator = aprovados.iterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("\nUsando Stream...");
+        Stream<String> stream = aprovados.stream();
+
+        stream.forEach(System.out::println);
+    }
+}
